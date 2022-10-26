bbe6d291b7ddf4c3d6c7260e74506c81b444f3b1
Matias Rodriguez <mat.rodri@gmail.com>
2022-10-26 01:49:06 -0300

    listo modulos de los componentes ItemListContainer/ItemList/Item

--- my-app/src/componentes/Item/Item.js
+++ my-app/src/componentes/Item/Item.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Item ({product}) {
   return (
     <>
-    
+    <div key={product.id} className ="wrapper">                        
+            <Link to={`/detalle/${product.id}`}>     
+                    
+                     <div className="card">
+                        <img src= {product.foto} alt="Foto producto" className="card__image"/>
+                        <div className="card__body">
+                            <h2 className="card__title">{product.name} </h2>
+                            <p className="card__category">{product.category}</p>
+                            <h3 className="card__price">{product.price}</h3>
+
+            
+                        </div> 
+                    </div> 
+            </Link>
+            
+    </div>
     </>
   )
 }
--- my-app/src/componentes/ItemList/ItemList.js
+++ my-app/src/componentes/ItemList/ItemList.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+
+import Item from '../Item/Item'
 
 
 const ItemList = ({products}) => {
   return (
     <div>
-            { products.map ( (product, index) => 
-            <div key={product.id} className ="wrapper">                        
-            <Link to={`/detalle/${product.id}`}>     
-                    
-                        <div className="card">
-                        <img src= {product.foto} alt="Foto producto" className="card__image"/>
-                        <div className="card__body">
-                            <h2 className="card__title">{product.name} </h2>
-                            <p className="card__category">{product.category}</p>
-                            <h3 className="card__price">{product.price}</h3>
-
-            
-                        </div> 
-                    </div> 
-            </Link>
-            
-         </div>)}
+            { products.map ( product => <Item key={product.id} product={product}/> )}
 </div> 
   )}
 
